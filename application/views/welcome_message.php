2fa49e3ec4693fc0f05967a04be0069f27ba668b
Alonso S <alonsosalcido13@gmail.com>
2016-07-26 13:13:04 -0600

    Hello world in codeIgniter

---
+++ application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Curso de CodigoFacilito</title>
+	<style>
+		body{
+			font-family: Arial, "Helvetica";
+		}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Hola mundo vía CodeIgniter</h1>
+</div>
+
+</body>
+</html>
